2f27c57845d9cefd850327a45f761ae462a0f340
Aliyou Sylla <aliyou29@hotmail.com>
2022-02-11 18:52:04 +0100

    Add test for empty build

--- shared/src/main/scala/steve/model.scala
+++ shared/src/main/scala/steve/model.scala
@@ -1,22 +1,28 @@
 package steve
 
-case class Build(
+enum Command {
+  case Build(build: steve.Build)
+  case Run(hash: Hash)
+}
+
+final case class Build(
   base: Build.Base,
-  commands: List[Command],
+  commands: List[Build.Command],
 )
 
 object Build {
 
   enum Base {
     case EmptyImage
-    case ImageHAsh(hash: Hash)
+    case ImageReference(hash: Hash)
   }
 
-}
+  enum Command {
+    case Upsert(key: String, value: String)
+    case Delete(key: String)
+  }
 
-enum Command {
-  case Upsert(key: String, value: String)
-  case Delete(key: String)
+  val empty: Build = Build(base = Build.Base.EmptyImage, commands = Nil)
 }
 
 final case class Hash(value: Array[Byte])
